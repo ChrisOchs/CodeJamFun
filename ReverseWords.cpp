e488e510ab0d78e72549416984996efdf24e8dff
Christopher Ochs <cro3@njit.edu>
2017-03-11 02:15:52 -0500

    Added solution to "reverse words" problem

---
+++ ReverseWords.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "ReverseWords.h"
+
+void reverseWords(std::string& str) {
+
+    std::string delimiter = " ";
+
+    std::vector<std::string> result;
+
+    splitString(str, delimiter, result);
+
+    std::reverse(result.begin(), result.end());
+
+    for(std::vector<std::string>::iterator it = result.begin(); it != result.end(); it++) {
+        std::cout<<*it;
+
+        if(it != result.end()) {
+            std::cout<<" ";
+        }
+    }
+}
+
+void splitString(std::string& str, std::string& delimiter, std::vector<std::string>& result) {
+    result.clear();
+
+    std::string input = str;
+    std::string split = delimiter;
+
+    size_t tokenPos = 0;
+
+    while((tokenPos = input.find(split)) != std::string::npos) {
+        result.push_back(input.substr(0, tokenPos));
+
+        input.erase(0, tokenPos + split.length());
+    }
+
+    result.push_back(input);
+}
---
+++ ReverseWords.h
@@ -0,0 +1,11 @@
+#ifndef REVERSEWORDS_H
+#define REVERSEWORDS_H
+
+#include <string>
+#include <vector>
+
+void reverseWords(std::string&);
+
+void splitString(std::string&, std::string&, std::vector<std::string>&);
+
+#endif // REVERSEWORDS_H
